77834b607cfbd3e90fe40769ef9fdf09db605a70
kizerkizer <kizerkizerkizerkizerkizer@gmail.com>
2018-09-09 00:57:57 +0000

    asd

--- sw/sw.js
+++ sw/sw.js
@@ -1,4 +1,23 @@
+let id = 0;
+
+const map = new Map();
+
 self.addEventListener('fetch', (event) => {
   console.log(event);
-  event.respondWith();
+  self.postMessage({
+    id: id++,
+    event
+  });
+  event.respondWith(new Promise((resolve, reject) => {
+    resolve('foo');
+    return;
+    map.set(id, {
+      resolve,
+      reject
+    });
+  });
+});
+
+self.addEventListener('message', (event) => {
+  console.log(event);
 });
