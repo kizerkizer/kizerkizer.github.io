4c465975026b6424c86b10a1ce048859dcc21968
kizerkizer <kizerkizerkizerkizerkizer@gmail.com>
2018-09-09 13:26:20 -0700

    dosf

--- sw/sw.js
+++ sw/sw.js
@@ -3,15 +3,14 @@ let id = 0;
 const map = new Map();
 
 self.addEventListener('fetch', (event) => {
-  let request = event.request;
   console.log(event);
-  event.respondWith(new Promise(resolve, reject) => {
-    resolve(new Response(request.url, {
+  event.respondWith(new Promise((resolve, reject) => {
+    resolve(new Response(event.request.url, {
       headers: {
         'Content-Type': 'text/plain'
       }
-    });
-  });
+    }))
+  }));
 });
 
 self.addEventListener('message', (event) => {
