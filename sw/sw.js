9c9ea3c69601cdfd399fa746c081809c1b327d4a
kizerkizer <kizerkizerkizerkizerkizer@gmail.com>
2018-09-09 01:12:07 +0000

    asd

--- sw/sw.js
+++ sw/sw.js
@@ -3,18 +3,23 @@ let id = 0;
 const map = new Map();
 
 self.addEventListener('fetch', (event) => {
+  let request = event.request;
   console.log(event);
   /*self.postMessage({
     id: id++,
     event
   });*/
   event.respondWith(new Promise((resolve, reject) => {
-    resolve('foo');
+    resolve(new Response(request.url, {
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    });
     return;
-    map.set(id, {
+    /*map.set(id, {
       resolve,
       reject
-    });
+    });*/
   }));
 });
 
