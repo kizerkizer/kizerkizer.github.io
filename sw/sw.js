8d2dddeb75edf1957f60e3f8e40489e2afd7d9c5
kizerkizer <kizerkizerkizerkizerkizer@gmail.com>
2018-09-09 20:18:00 +0000

    asdf

--- sw/sw.js
+++ sw/sw.js
@@ -5,22 +5,13 @@ const map = new Map();
 self.addEventListener('fetch', (event) => {
   let request = event.request;
   console.log(event);
-  /*self.postMessage({
-    id: id++,
-    event
-  });*/
-  event.respondWith(new Promise((resolve, reject) => {
+  event.respondWith(new Promise(resolve, reject) => {
     resolve(new Response(request.url, {
       headers: {
         'Content-Type': 'text/plain'
       }
     });
-    return;
-    /*map.set(id, {
-      resolve,
-      reject
-    });*/
-  }));
+  });
 });
 
 self.addEventListener('message', (event) => {
